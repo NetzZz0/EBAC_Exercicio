a195a17668c6f0ac7f3aab2f0775c149d281c326
NetzZz0 <enzong10@gmail.com>
2024-08-15 18:15:19 -0300

    Comparando campos

--- Exercicio modulo 7/main.js
+++ Exercicio modulo 7/main.js
@@ -1,1 +1,20 @@
 
+const form = document.getElementById('form-campos')
+const campoA = document.getElementById('campo-A');
+const campoB = document.getElementById('campo-B');
+
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const valorA = Number(campoA.value)
+    const valorB = Number(campoB.value)
+
+    if (valorB > valorA) {
+        document.querySelector('.success-msg').style.display = 'block';
+
+        campoA.value = '';
+        campoB.value = '';
+    } else {
+        document.querySelector('.error-msg').style.display = 'block';
+    }
+})
